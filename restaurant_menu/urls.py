05eb5e745d262e7e18244477be783892e896909f
GeVhoo <gvahrushev@mail.ru>
2020-08-11 00:13:29 +0500

    Update settings

--- restaurant_menu/urls.py
+++ restaurant_menu/urls.py
@@ -1,5 +1,3 @@
-"""restaurant_menu URL Configuration"""
-
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -9,7 +7,9 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('menu.urls')),
+    path('api/', include('menu_api.urls')),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
